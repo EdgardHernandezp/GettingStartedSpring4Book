c9215dd5f907ec1d8475146005bf652cf0a20c2a
Dreamseeker5 <edgardhernandezp@gmail.com>
2019-11-18 20:11:37 -0400

    Made the app redirect the output to the console.

--- src/main/java/com/dreamseeker/GettingStartedSpringBookApplication.java
+++ src/main/java/com/dreamseeker/GettingStartedSpringBookApplication.java
@@ -1,13 +1,25 @@
 package com.dreamseeker;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.dreamseeker.beans.SgtPeppers;
 
 @SpringBootApplication
-public class GettingStartedSpringBookApplication {
+public class GettingStartedSpringBookApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GettingStartedSpringBookApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		try (AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(CDPlayerConfig.class)) {
+			SgtPeppers bean = ctx.getBean(SgtPeppers.class);
+			bean.play();
+		}
 	}
 
 }
